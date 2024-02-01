3ffed92878c73c3c9e138253bead28d483799e11
ASWIN-PALAYAD <aswinpalayad07@gmail.com>
2024-02-01 22:13:16 +0530

    create listing item component and show listings

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import ListingItem from "../components/ListingItem";
 
 
 const Search = () => {
@@ -177,11 +178,22 @@ const Search = () => {
         </div>
 
 
-        <div className=''>
+        <div className='flex-1'>
             <h1 className='text-3xl font-semibold border-b p-3 text-slate-700
             mt-5'>
                 Listing Result
             </h1>
+            <div className="p-7 flex flex-wrap gap-4">
+                {!loading && listing?.length === 0 && (
+                    <p className="text-xl text-slate-700 ">No listing found....!</p>
+                )}
+                {loading && (
+                    <p className="text-xl text-slate-700 text-center w-full">Loading...</p>
+                )}
+                {!loading && listing && listing.map((listing)=> (
+                    <ListingItem key={listing._id} listing={listing}/>
+                ))}
+            </div>
         </div>
     </div>
   )
