6a4aff7d8fcba93a8c42ca93b3f0ac0a498a2819
ASWIN-PALAYAD <aswinpalayad07@gmail.com>
2024-02-02 13:04:38 +0530

    vercel file added

--- api/index.js
+++ api/index.js
@@ -7,7 +7,11 @@ import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listingRoutes.js';
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://myproperty.vercel.app/',
+  methods: ["POST", "GET", "PUT", "DELETE"],
+  credentials: true
+}));
 
 
 app.use(express.json());
